8dee8cf94040dfff8ae30f8a6421cd8115b7812f
Philip Bradley <pdbradley@gmail.com>
2024-09-10 10:07:49 +0000

    cursor generated key generator

---
+++ app/lib/key_generator.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class KeyGenerator
+  def self.generate
+    letters = ('A'..'Z').to_a.sample(4).join
+    numbers = (0..9).to_a.sample(4).join
+    "#{letters}-#{numbers}"
+  end
+end
