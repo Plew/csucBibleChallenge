5e7b9daae69c7041602468ae104121bec3be129f
Philip Bradley <pdbradley@gmail.com>
2024-09-28 11:24:39 -0400

    better earth icon

--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -4,20 +4,17 @@ class GroupMembershipsController < ApplicationController
   end
 
   def create
-    @group = Group.find_by(key: params[:key])
-    @group_membership = @group.group_memberships.build(user: current_user)
-    @group_membership.save
+    group = Group.find_by(key: params[:key])
+    group_membership = group.group_memberships.build(user: current_user)
+    group_membership.save
     # redirect_to @group
     redirect_to groups_path
   end
 
   def destroy
-    @group_membership = @group.group_memberships.find_by(user: current_user)
-    if @group_membership&.destroy
-      redirect_to @group, notice: 'You have left the group.'
-    else
-      redirect_to @group, alert: 'Unable to leave the group.'
-    end
+    group_membership = GroupMembership.find_by(user: current_user, group_id: params[:group_id])
+    group_membership&.destroy
+    redirect_to groups_path
   end
 
 end
