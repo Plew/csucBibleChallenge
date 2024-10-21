f0976209919a08c50ed6d68f66ba5aafc91f7c8d
Philip Bradley <pdbradley@gmail.com>
2024-10-21 13:29:42 +0000

    skip auth token in dev mode for codespaces to work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :set_active_date
   before_action :set_current_date
 
+  if Rails.env.development?
+    skip_before_action :verify_authenticity_token
+  end
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
   helper_method :current_user
