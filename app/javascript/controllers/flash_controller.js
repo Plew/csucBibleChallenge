278b82840eb3dfa8385686f515131db0ef1c5b9a
Philip Bradley <pdbradley@gmail.com>
2024-10-24 09:13:42 -0400

    update flash message (still not great)

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,12 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     setTimeout(() => {
-      this.element.style.transition = "opacity 0.5s ease"
+      this.element.style.transition = "opacity 0.75s ease"
       this.element.style.opacity = 0
       
       setTimeout(() => {
         this.element.remove()
-      }, 500)
-    }, 3000)
+      }, 2000)
+    }, 1500)
   }
 }
