8b4f3b1ee8adb267ed67bb4d5b16e4d377abde28
Philip Bradley <pdbradley@gmail.com>
2024-10-24 07:39:27 -0400

    primitive fading flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when "notice" then "bg-blue-100 text-blue-700"
+    when "success" then "bg-green-100 text-green-700"
+    when "error" then "bg-red-100 text-red-700"
+    when "alert" then "bg-yellow-100 text-yellow-700"
+    end
+  end
 end
