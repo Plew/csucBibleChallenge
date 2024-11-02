79880c366f1662a019b23a4e99777525e7e683b8
Philip Bradley <pdbradley@gmail.com>
2024-11-02 05:34:57 -0400

    try to constrain flash horizontally

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
-  def flash_class(level)
-    base_classes = "absolute top-0 left-0 right-0 z-50 p-4 text-center w-full"
-    case level
-    when "notice" then "#{base_classes} bg-blue-100 text-blue-700 mt-8 mx-8"
-    when "success" then "#{base_classes} bg-green-100 text-green-700 mt-8 mx-8"
-    when "error" then "#{base_classes} bg-red-100 text-red-700 mt-8 mx-8"
-    when "alert" then "#{base_classes} bg-yellow-100 text-yellow-700 mt-8 mx-8"
+  def flash_class(key)
+    base_classes = "px-4 py-3 mx-4 my-2 rounded relative text-center"
+    
+    case key.to_sym
+    when :notice, :success
+      "#{base_classes} bg-green-100 text-green-700 border border-green-400"
+    when :error, :alert
+      "#{base_classes} bg-red-100 text-red-700 border border-red-400"
+    else
+      "#{base_classes} bg-blue-100 text-blue-700 border border-blue-400"
     end
   end
 end
