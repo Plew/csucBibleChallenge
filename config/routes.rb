c5e54fea4e66103bac4ae454b0296c38fd09d0d4
Philip Bradley <pdbradley@gmail.com>
2025-01-02 07:40:45 -0500

    wording

--- config/routes.rb
+++ config/routes.rb
@@ -34,4 +34,10 @@ Rails.application.routes.draw do
     mount Lookbook::Engine, at: "/lookbook"
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :group_statistics, only: [:index, :show]
+    end
+  end
+
 end
