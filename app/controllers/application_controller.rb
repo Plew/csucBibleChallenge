74fe8f5c1ddda945ae7cca1184dcbb667e3bed19
Philip Bradley <pdbradley@gmail.com>
2025-01-05 16:32:31 -0500

    no server side backup when browser date cookies absent
    also cleaned up app controller to make it simpler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :set_browser_date
+  before_action :set_current_browser_date
 
   if Rails.env.development?
     skip_before_action :verify_authenticity_token
@@ -17,23 +17,17 @@ class ApplicationController < ActionController::Base
     @current_user ||= current_device.user || User.create!.tap { |user| user.devices << current_device }
   end
 
-  def browser_date
-    # this cookie is set by application.js when the page is loaded
-    # it is only set in application.js
-    if cookies[:browser_date].present?
-      Date.parse(cookies[:browser_date])
-    else
-      Date.today
-    end
-  end
-
   private
 
   def current_device_id
     cookies.permanent[:device_id] ||= SecureRandom.uuid
   end
 
-  def set_browser_date
-    Current.browser_date = browser_date
+  def set_current_browser_date
+    if cookies[:browser_date].present?
+      Current.browser_date = Date.parse(cookies[:browser_date])
+    else
+      raise "browser_date cookie not set"
+    end
   end
 end
