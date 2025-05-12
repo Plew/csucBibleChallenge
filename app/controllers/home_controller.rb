893f10a474c89c9c9f625d29b15bebf05364a325
Philip Bradley <pdbradley@gmail.com>
2025-05-12 11:21:27 -0400

    Refactor home#index to use ReadingDisplayComponent for today's reading verses display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,10 @@ class HomeController < ApplicationController
     if @user_challenge
       today_in_challenge_tz = Time.current.in_time_zone(@user_challenge.timezone).to_date
       @todays_reading = @user_challenge.readings.find_by(scheduled_date: today_in_challenge_tz)
+      if @todays_reading
+        @todays_reading_title = @todays_reading.title
+        @todays_reading_verses = @todays_reading.verses.map { |v| { verse_number: v.verse_number, verse_text: v.verse_text } }
+      end
     end
     # The placeholder @today_readings is removed.
   end
