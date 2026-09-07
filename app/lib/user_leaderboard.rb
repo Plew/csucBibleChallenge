42f83a7dea216a0fb5d1897153e84730cf1b33ba
Peter Watson <plewwatson@gmail.com>
2026-09-07 10:24:48 -0400

    fixed lint

--- app/lib/user_leaderboard.rb
+++ app/lib/user_leaderboard.rb
@@ -11,7 +11,7 @@ class UserLeaderboard
     return [] if total_readings.zero?
 
     sql = <<~SQL
-      SELECT users.*, 
+      SELECT users.*,#{' '}
              COALESCE(completions.count, 0) as completed_count,
              ROUND(COALESCE(completions.count, 0) * 100.0 / #{total_readings}, 1) as completion_percentage
       FROM users
@@ -42,7 +42,7 @@ class UserLeaderboard
 
     effective_end = [ challenge.effective_stats_end_date, current_date_in_challenge_timezone ].min
     sql = <<~SQL
-      SELECT users.*, 
+      SELECT users.*,#{' '}
              COALESCE(completions.count, 0) as completed_to_date_count,
              ROUND(COALESCE(completions.count, 0) * 100.0 / #{readings_to_date}, 1) as on_track_percentage
       FROM users
@@ -70,7 +70,7 @@ class UserLeaderboard
   # Returns users ordered by total reading count (highest first)
   def by_total_readings
     sql = <<~SQL
-      SELECT users.*, 
+      SELECT users.*,#{' '}
              COALESCE(completions.count, 0) as total_completed
       FROM users
       INNER JOIN user_challenge_enrollments uce ON uce.user_id = users.id AND uce.challenge_id = #{challenge.id}
@@ -102,13 +102,13 @@ class UserLeaderboard
     sql = <<~SQL
       WITH RECURSIVE streak_calc AS (
         -- Base case: Start from current date for each user
-        SELECT 
+        SELECT#{' '}
           users.id as user_id,
           users.username,
           '#{effective_end}' as check_date,
-          CASE 
-            WHEN completed_readings.reading_date = '#{effective_end}' THEN 1 
-            ELSE 0 
+          CASE#{' '}
+            WHEN completed_readings.reading_date = '#{effective_end}' THEN 1#{' '}
+            ELSE 0#{' '}
           END as current_streak,
           0 as iteration
         FROM users
@@ -116,22 +116,22 @@ class UserLeaderboard
         LEFT JOIN (
           SELECT ur.user_id, r.scheduled_date as reading_date
           FROM user_readings ur
-          INNER JOIN readings r ON r.id = ur.reading_id 
-          WHERE r.challenge_id = #{challenge.id} 
+          INNER JOIN readings r ON r.id = ur.reading_id#{' '}
+          WHERE r.challenge_id = #{challenge.id}#{' '}
             AND r.scheduled_date >= '#{challenge.effective_stats_start_date}'
             AND r.scheduled_date <= '#{effective_end}'
-        ) completed_readings ON completed_readings.user_id = users.id 
+        ) completed_readings ON completed_readings.user_id = users.id#{' '}
                               AND completed_readings.reading_date = '#{effective_end}'
-        
+      #{'  '}
         UNION ALL
-        
+      #{'  '}
         -- Recursive case: Check previous days
-        SELECT 
+        SELECT#{' '}
           sc.user_id,
           u.username,
           date(sc.check_date, '-1 day'),
-          CASE 
-            WHEN cr.reading_date = date(sc.check_date, '-1 day') AND sc.current_streak > 0 
+          CASE#{' '}
+            WHEN cr.reading_date = date(sc.check_date, '-1 day') AND sc.current_streak > 0#{' '}
             THEN sc.current_streak + 1
             ELSE 0
           END,
@@ -141,21 +141,21 @@ class UserLeaderboard
         LEFT JOIN (
           SELECT ur.user_id, r.scheduled_date as reading_date
           FROM user_readings ur
-          INNER JOIN readings r ON r.id = ur.reading_id 
+          INNER JOIN readings r ON r.id = ur.reading_id#{' '}
           WHERE r.challenge_id = #{challenge.id}
             AND r.scheduled_date >= '#{challenge.effective_stats_start_date}'
             AND r.scheduled_date <= '#{effective_end}'
         ) cr ON cr.user_id = sc.user_id AND cr.reading_date = date(sc.check_date, '-1 day')
-        WHERE sc.current_streak > 0 
+        WHERE sc.current_streak > 0#{' '}
           AND date(sc.check_date, '-1 day') >= '#{challenge.effective_stats_start_date}'
           AND sc.iteration < 365  -- Prevent infinite recursion
       ),
       user_max_streaks AS (
-        SELECT 
+        SELECT#{' '}
           user_id,
           username,
           MAX(current_streak) as current_streak
-        FROM streak_calc 
+        FROM streak_calc#{' '}
         GROUP BY user_id, username
       )
       SELECT users.*, COALESCE(ums.current_streak, 0) as current_streak
